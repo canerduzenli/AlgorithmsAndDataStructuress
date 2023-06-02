0b433caf88f12c88373b78ea0280f9bc74e3f0c3
Caner Duzenli <122194561+canerduzenli@users.noreply.github.com>
2023-06-01 22:52:02 -0500

    Delete Lab_2.cs

--- AlgorithmsAndDataStructuress/Lab_2.cs
+++
@@ -1,8 +0,0 @@
-﻿
-
-string str = "Programmatic Python";
-var result = str.ToLower()
-                .GroupBy(c => c)
-                .Where(g => g.Count() > 1)
-                .Select(g => g.Key)
-                .ToArray();
