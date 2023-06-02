5a20085bcd30b7e6d254a151a7b14a0478a61e73
caner <caner@CANER>
2023-06-01 20:15:52 -0500

    updated

--- AlgorithmsAndDataStructuress/Program.cs
+++ AlgorithmsAndDataStructuress/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+Console.WriteLine("Please enter a number :");
+int n = int.Parse(Console.ReadLine());
+
+string[] words = new string[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Please enter a word:");
+    words[i] = Console.ReadLine();
+}
+
+Console.WriteLine("Please enter a character:");
+char c = Console.ReadKey().KeyChar;
+
+int charCount = 0;
+int totalCharCount = 0;
+
+foreach (string word in words)
+{
+    foreach (char wordChar in word)
+    {
+        if (Char.ToLower(wordChar) == Char.ToLower(c))
+        {
+            charCount++;
+        }
+        totalCharCount++;
+    }
+}
+
+Console.WriteLine($"\nThe letter '{c}' appears {charCount} times in the array.");
+
+double percentage = ((double)charCount / totalCharCount) * 100;
+if (percentage > 25)
+{
+    Console.WriteLine("This letter makes up more than 25% of the total number of characters.");
+}
+else
+{
+    Console.WriteLine("This letter makes up less than 25% of the total number of characters.");
+}
