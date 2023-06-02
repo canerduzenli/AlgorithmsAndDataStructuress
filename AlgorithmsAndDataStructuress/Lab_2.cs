4b7966c9e94e66dc3a89c2a2fe76dbf558d8b05d
caner <caner@CANER>
2023-06-01 21:11:57 -0500

    lab2

---
+++ AlgorithmsAndDataStructuress/Lab_2.cs
@@ -0,0 +1,8 @@
+﻿
+
+string str = "Programmatic Python";
+var result = str.ToLower()
+                .GroupBy(c => c)
+                .Where(g => g.Count() > 1)
+                .Select(g => g.Key)
+                .ToArray();
